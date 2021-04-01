d225e421bacb5154b0562a6bf602231cf3419b62
Andrew Robinson <sirarkimedes@gmail.com>
2021-04-01 17:50:20 -0600

    Create a auth.json path to the authentication key

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(auth.token);
