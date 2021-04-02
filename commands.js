cd272cb5ffb2e790245834247ca69099ba91fe9b
Andrew Robinson <sirarkimedes@gmail.com>
2021-04-01 23:16:11 -0600

    Get a mention list setup and synced

--- commands.js
+++ commands.js
@@ -1,3 +1,4 @@
+const { readInFile, writeFile, MENTION_LIST_FILE_PATH} = require('./file_reader.js');
 
 function attemptCommandEvaluation(message) {
     var messageContent = message.content;
@@ -9,11 +10,47 @@ function attemptCommandEvaluation(message) {
 };
 
 function addMemberToMentionList(message) {
-    console.log('worked!');
-}
+    readInFile(MENTION_LIST_FILE_PATH, (data) => {
+        const userIds = message.mentions.users.map(user => {
+            return user.id
+        });
+        
+        var mentionsList = JSON.parse(data);
+        for (let id of userIds) {
+            if (!mentionsList.includes(id)) {
+                mentionsList.push(id);
+            }
+        }
+        
+        writeFile(MENTION_LIST_FILE_PATH, JSON.stringify(mentionsList), (succeeded) => {
+            if (succeeded) {
+                message.react('✅');
+            }
+        });
+    });
+};
 
 function removeMemberFromMentionList(message) {
-    console.log('worked! 2');
-}
+    readInFile(MENTION_LIST_FILE_PATH, (data) => {
+        const userIds = message.mentions.users.map(user => {
+            return user.id
+        });
+        
+        var mentionsList = JSON.parse(data);
+        for (let id of userIds) {
+            if (mentionsList.includes(id)) {
+                mentionsList = mentionsList.filter((value, index, array) => {
+                    return value !== id;
+                });
+            }
+        }
+        
+        writeFile(MENTION_LIST_FILE_PATH, JSON.stringify(mentionsList), (succeeded) => {
+            if (succeeded) {
+                message.react('✅');
+            }
+        });
+    });
+};
 
 exports.attemptCommandEvaluation = attemptCommandEvaluation;
---
+++ file_reader.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const MENTION_LIST_FILE_PATH = './mentionList.json'
+
+function readInFile(filePath, callback) {
+    fs.readFile(filePath, 'utf8', (error, data) => {
+        if (error) {
+            console.log(`Error reading file from disk: ${error}`);
+        } else {
+            callback(data);
+        }
+    });
+};
+
+function writeFile(filePath, data, callback) {
+    fs.writeFile(filePath, data, 'utf8', (error) => {
+        if (error) {
+            console.log(`Error writing file: ${error}`);
+        } else {
+            callback(true);
+        }
+    });
+};
+
+exports.MENTION_LIST_FILE_PATH = MENTION_LIST_FILE_PATH;
+exports.readInFile = readInFile;
+exports.writeFile = writeFile;
