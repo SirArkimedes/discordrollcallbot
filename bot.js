008e4f5fef1b840c1a05495cf97aeacefa7c9c01
Andrew Robinson <sirarkimedes@gmail.com>
2021-04-01 23:49:17 -0600

    @ mention the people that should get @ mention'd

--- bot.js
+++ bot.js
@@ -18,32 +18,49 @@ client.on('message', message => {
 });
 
 function inOrOutPing(message) {
-  const embed = new MessageEmbed()
-    .setTitle('Who\'s in?')
-    .setColor('0xffe000')
-    .setDescription('React to this message to mark that you\'re in or out for tonight!');
+  readInFile(MENTION_LIST_FILE_PATH, (data) => {
+    const mentionsList = JSON.parse(data);
+    var thoseToMention = '';
+    for (i = 0; i < mentionsList.length; i++) {
+      if (mentionsList.length != 1 && i == mentionsList.length - 1) {
+        thoseToMention += ' and '
+      }
+      
+      thoseToMention += `<@${mentionsList[i]}>`
 
-  message.channel.send(embed)
-    .then((embededMessage) => {
-      embededMessage.react('👍');
-      embededMessage.react('👎');
+      if (mentionsList.length != 1 && i != mentionsList.length - 1 && mentionsList.length != 2) {
+        thoseToMention += ', '
+      }
+    }
 
-      const filter = (reaction, user) => {
-        return ['👍', '👎'].includes(reaction.emoji.name) && user.id !== embededMessage.author.id;
-      };
-      embededMessage.createReactionCollector(filter, { time: 15000, dispose: true })
-        .on('collect', (reaction, user) => {
-          if (reaction.emoji.name === '👍') {
-            embededMessage.channel.send(`🙋 <@${user.id}> is in!`);
-          } else if (reaction.emoji.name === '👎') {
-            embededMessage.channel.send(`😢 <@${user.id}> is out!`);
-          }
-        })
-        .on('remove', (reaction, user) => {
-          embededMessage.channel.send(`<@${user.id}> removed their choice!`);
-        })
-        .on('end', collected => console.log(`Collected ${collected.size} items`));
-    });
+    const embedMessage = new MessageEmbed()
+      .setTitle('Who\'s in?')
+      .setColor('0xffe000')
+      .setDescription('React to this message to mark that you\'re in or out for tonight!');
+    const messageContent = `${thoseToMention} are you in?`;
+
+    message.channel.send({ content: messageContent, embed: embedMessage })
+      .then((embededMessage) => {
+        embededMessage.react('👍');
+        embededMessage.react('👎');
+
+        const filter = (reaction, user) => {
+          return ['👍', '👎'].includes(reaction.emoji.name) && user.id !== embededMessage.author.id;
+        };
+        embededMessage.createReactionCollector(filter, { time: 15000, dispose: true })
+          .on('collect', (reaction, user) => {
+            if (reaction.emoji.name === '👍') {
+              embededMessage.channel.send(`🙋 <@${user.id}> is in!`);
+            } else if (reaction.emoji.name === '👎') {
+              embededMessage.channel.send(`😢 <@${user.id}> is out!`);
+            }
+          })
+          .on('remove', (reaction, user) => {
+            embededMessage.channel.send(`<@${user.id}> removed their choice!`);
+          })
+          .on('end', collected => console.log(`Collected ${collected.size} items`));
+      });
+  })
 };
 
 client.login(auth.token);
